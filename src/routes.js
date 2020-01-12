cca422124e229f981ec182f2a0510fe535d0ee0c
JC <jcroughton@gmail.com>
2020-01-12 21:36:51 +1000

    Adds initial media detail view and route

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,10 @@ export const routes = [
     path: "/",
     component: lazy(() => import('./views/home/Home')),
     exact: true
+  },
+  {
+    path: "/media-detail/:mediaId",
+    component: lazy(() => import('./views/media/MediaDetail')),
+    exact: true
   }
 ];
---
+++ src/views/media/MediaDetail.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+
+export class MediaDetail extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h2>Media detail</h2>
+      </Fragment>
+    )
+  };
+}
+
+export default MediaDetail;
