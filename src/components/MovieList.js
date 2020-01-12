c7cda861e826ffeeab6ff1db9a4518d3a36b9f6b
JC <jcroughton@gmail.com>
2020-01-12 16:32:51 +1000

    Initial movie list component

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,7 +3,27 @@ import React, { Component } from 'react'
 export class MovieList extends Component {
   render() {
     return (
-      <p>Movies will be listed here</p>
+      <ul>
+        {
+          Object.keys(this.props.moviesToList).map((movieId) =>
+            <li key={movieId}>
+              <div>
+              {
+                this.props.moviesToList[movieId].backdrop_path !== null ?
+                  <img src={`https://image.tmdb.org/t/p/w500${this.props.moviesToList[movieId].backdrop_path}`} alt={`${this.props.moviesToList[movieId].title} poster`} />
+                :
+                  "No poster image available."
+              }
+              </div>
+              <div>
+                <h3>{this.props.moviesToList[movieId].title}</h3>
+                <p>{this.props.moviesToList[movieId].overview}</p>
+                <p>Released: {this.props.moviesToList[movieId].release_date}</p>
+              </div>
+            </li>
+          )
+        }
+      </ul>
     )
   };
 }
