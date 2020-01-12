da3677ad11d79f79a894cf07d9f120e843774393
JC <jcroughton@gmail.com>
2020-01-12 21:25:37 +1000

    Wrap image with link to movie detail page

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import { Link } from 'react-router-dom';
 export class MovieList extends Component {
   render() {
     return (
@@ -7,14 +7,14 @@ export class MovieList extends Component {
         {
           Object.keys(this.props.moviesToList).map((movieId) =>
             <li key={movieId}>
-              <div>
+              <Link to={`/movie-detail/${this.props.moviesToList[movieId].id}`}>
               {
                 this.props.moviesToList[movieId].backdrop_path !== null ?
                   <img src={`https://image.tmdb.org/t/p/w500${this.props.moviesToList[movieId].backdrop_path}`} alt={`${this.props.moviesToList[movieId].title} poster`} />
                 :
                   "No poster image available."
               }
-              </div>
+              </Link>
               <div>
                 <h3>{this.props.moviesToList[movieId].title}</h3>
                 <p>{this.props.moviesToList[movieId].overview}</p>
