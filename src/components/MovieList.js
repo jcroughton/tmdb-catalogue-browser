c9f915e95e301dd26f39af6a4d68afa97a645b75
JC <jcroughton@gmail.com>
2020-01-12 16:16:28 +1000

    Adds initial movie list component

---
+++ src/components/MovieList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class MovieList extends Component {
+  render() {
+    return (
+      <p>Movies will be listed here</p>
+    )
+  };
+}
+
+export default MovieList;
