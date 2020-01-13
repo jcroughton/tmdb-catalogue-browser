e86bd4bb8c8ac0e554c8f2af59d550e31502e5b7
JC <jcroughton@gmail.com>
2020-01-13 22:40:38 +1000

    Adds purgecss configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,47 @@
+const purgecss = require('@fullhuman/postcss-purgecss');
+
+/**
+ * Return an array of regex patterns defining CSS classes to be whitelisted
+ * during CSS purge
+ *
+ * @return  {Array}
+ */
+function collectPurgeWhitelistPatterns() {
+  // do something to collect the whitelist
+  return [
+    /fill-current/g,
+    /stroke-current/g,
+  ];
+}
+
 module.exports = {
   parser: 'postcss-scss',
   plugins: [
     require('postcss-import'),
+    require('tailwindcss'),
     require('autoprefixer'),
+    purgecss({
+      keyframes: true,
+      fontFace: true,
+      content: [
+        './src/**/*.html', './src/**/*.js'
+      ],
+      css: [
+        './src/**/*.css'
+      ],
+      extractors: [
+        {
+          extractor: class TailwindExtractor {
+            static extract(content) {
+              const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '');
+              return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
+            }
+          },
+          extensions: ['html', 'js'],
+        },
+      ],
+      whitelistPatterns: collectPurgeWhitelistPatterns(),
+      whitelistPatternsChildren: collectPurgeWhitelistPatterns(),
+    })
   ]
 }
