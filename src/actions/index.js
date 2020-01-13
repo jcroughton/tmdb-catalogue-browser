154dca848fa24573b151b75aed502bbc88eb7872
JC <jcroughton@gmail.com>
2020-01-13 20:46:39 +1000

    Adds 'adding' popular movies (to store) to actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,9 @@
+import { ADD_POPULAR_MOVIES } from 'constants/action-types';
 import { SEARCH_MOVIES } from 'constants/action-types';
+
+export function addPopularMovies (payload) {
+  return { type: ADD_POPULAR_MOVIES, payload };
+};
 
 export function searchMovies (payload) {
   return { type: SEARCH_MOVIES, payload };
