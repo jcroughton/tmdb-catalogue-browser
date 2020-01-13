4c182b3c5f4f20b383c111a0551432064adb3304
JC <jcroughton@gmail.com>
2020-01-13 21:01:37 +1000

    Adds popular mivies component

---
+++ src/components/PopularMovies.js
@@ -0,0 +1,46 @@
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux';
+
+import { addPopularMovies } from '../actions/index';
+
+import MovieList from '../components/MovieList';
+
+const mapStateToProps = state => {
+  return {
+    popularMovies: state.popularMovies
+  }
+};
+
+const matchDispatchToProps = dispatch => {
+  return {
+    addPopularMovies: popularMovies => dispatch(addPopularMovies(popularMovies))
+  };
+};
+
+export class PopularMovies extends Component {
+  componentDidMount() {
+    fetch(`${process.env.REACT_APP_API_BASE_URL}/movie/popular?api_key=${process.env.REACT_APP_TMDB_API_KEY}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          this.props.addPopularMovies(result.results);
+        },
+        (error) => {
+          throw Error('Error retrieving popular movies.');
+        }
+      )
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <h2>Popular movies</h2>
+
+        <MovieList moviesToList={this.props.popularMovies} />
+      </Fragment>
+    )
+  }
+}
+
+export default connect(mapStateToProps, matchDispatchToProps)(PopularMovies);
