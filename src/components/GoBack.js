da491a8f3fac789d00a601c1db19f5ecb726eb1e
JC <jcroughton@gmail.com>
2020-01-14 23:34:29 +1000

    Amends back link to be arrow

--- src/components/GoBack.js
+++ src/components/GoBack.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
+import backArrow from 'images/ic_arrow_back_white.svg';
+
 export default function GoBack() {
   let history = useHistory();
 
   return (
-    <button type="button" onClick={() => history.goBack()}>
-      Get back...
-    </button>
+    <img
+      className="
+        absolute
+        cursor-pointer
+        ml-5
+        mt-8
+      "
+      src={backArrow} alt="Back" onClick={() => history.goBack()}
+    />
   )
 };
