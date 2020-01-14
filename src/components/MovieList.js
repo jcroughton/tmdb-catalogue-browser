eb85b4cdadf69a49db82d90c991ecca39e2b736b
JC <jcroughton@gmail.com>
2020-01-14 21:38:42 +1000

    Style movie cards

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -16,24 +16,38 @@ export class MovieList extends Component {
               key={movieId}
               className=
                 {
-                  `w-6/12
+                  `
+                  Card
+                  w-6/12
                   ${(index % 2) ? 'pl-4' : 'pr-4'}
                   `
                 }
             >
-              <Link to={`/media-detail/${this.props.moviesToList[movieId].id}`}>
+              <Link className="block" to={`/media-detail/${this.props.moviesToList[movieId].id}`}>
               {
                 this.props.moviesToList[movieId].poster_path !== null ?
-                  <img src={`https://image.tmdb.org/t/p/w500${this.props.moviesToList[movieId].poster_path}`} alt={`${this.props.moviesToList[movieId].title} poster`} />
+                  <div className="relative">
+                    <img className="Card__image" src={`https://image.tmdb.org/t/p/w500${this.props.moviesToList[movieId].poster_path}`} alt={`${this.props.moviesToList[movieId].title} poster`} />
+                    <span
+                      className=
+                      {
+                        `Card__popularity
+                        ${(parseFloat(this.props.moviesToList[movieId].vote_average) >= 7) ? 'bg-charlie' : null}
+                        ${(parseFloat(this.props.moviesToList[movieId].vote_average) < 7) ? 'bg-foxtrot' : null}
+                        ${(parseFloat(this.props.moviesToList[movieId].vote_average) < 3) ? 'bg-golf' : null}
+                        `
+                      }
+
+                      >
+                        <Popularity score={this.props.moviesToList[movieId].vote_average} /></span>
+                  </div>
                 :
                   "No poster image available."
               }
               </Link>
-              <div>
-                <h3>{this.props.moviesToList[movieId].title}</h3>
-                <p>{moment(this.props.moviesToList[movieId].release_date).format('MMMM YYYY')}</p>
-
-                <p><Popularity score={this.props.moviesToList[movieId].vote_average} /></p>
+              <div className="Card__details">
+                <h2 className="Card__title">{this.props.moviesToList[movieId].title}</h2>
+                <span className="Card__date">{moment(this.props.moviesToList[movieId].release_date).format('MMMM YYYY')}</span>
               </div>
             </li>
           )
